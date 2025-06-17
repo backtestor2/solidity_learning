763beb7c1148eeb69f7f00a3d6ed7ba381e16d3d
backtestor2 <backtestor@gmail.com>
2025-06-17 05:29:20 +0000

    feat: added owner control to storage

--- Storage.sol
+++ Storage.sol
@@ -3,8 +3,18 @@ pragma solidity ^0.8.0;
 
 contract Storage {
     uint256 public number;
+    address public owner;
 
-    function store(uint256 _number) public {
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier isOwner() {
+        require(msg.sender == owner, "Only contract owner can store data");
+        _;
+    }
+
+    function store(uint256 _number) public isOwner {
         number = _number;
     }
 }
